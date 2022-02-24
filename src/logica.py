fe150541d37b701238c389d1b67e1112f966a93b
Polaryti <mario.camposmocholi@gmail.com>
2022-02-24 22:07:18 +0100

    Algo

--- src/logica.py
+++ src/logica.py
@@ -1,4 +1,5 @@
 from sqlalchemy import true
+
 from input import input_process
 from output import output_process
 
@@ -8,7 +9,7 @@ def skill_possible(project_skills, pearson_skills):
         for pearson_skill_, score in pearson_skills.items():
             if project_skill == pearson_skill_ and skill_score <= score:
                 return project_skill
-    return False
+    return []
 
 
 def is_possible(project, people, schedule, day, duration):
@@ -16,11 +17,9 @@ def is_possible(project, people, schedule, day, duration):
     for p, skills in people.items():
         if day not in schedule[p]:
             sk = skill_possible(project['roles'], skills[0])
-            if not sk:
-                pass
-            else:
+            if sk != []:
                 workers[list(project['roles'].keys()).index(sk)] = p
-                del project['roles'][sk]
+                project['roles'].pop(sk)
                 for i in range(duration):
                     schedule[p][day + i] = 1
         if project['roles'] == {} and 0 not in workers and workers != []:
@@ -32,32 +31,32 @@ def is_possible(project, people, schedule, day, duration):
 if __name__ == "__main__":
     schedule = {}
     solution = {}
-    file = 'data/f_find_great_mentors.in.txt'
+    file = 'data/b_better_start_small.in.txt'
 
     people, projects = input_process(file)
     for p in list(people.keys()):
         schedule[p] = {}
 
-    # first_iteration = True
-    # to_delete = []
-    # i = 100
-    # while first_iteration or i > 0:
-        # i -= 1
-        # first_iteration = False
-    day = 0
+    first_iteration = True
     to_delete = []
-    for pro in list(projects.values()):
-        if pro['name'] not in to_delete:
-            duration = int(pro['days'])
-            res = is_possible(pro, people, schedule, day, duration)
+    i = 100
+    while first_iteration or i > 0:
+        i -= 1
+        first_iteration = False
+        day = 0
+        to_delete = []
+        for pro in list(projects.values()):
+            if pro['name'] not in to_delete:
+                duration = int(pro['days'])
+                res = is_possible(pro, people, schedule, day, duration)
 
-            if res is not False:
-                solution[pro['name']] = res
-                to_delete.append(pro['name'])
+                if res is not False:
+                    solution[pro['name']] = res
+                    to_delete.append(pro['name'])
 
-            day += duration
+                day += duration
 
-        # for delete in to_delete:
-        #     del projects[delete]
+        for delete in to_delete:
+            projects.pop(delete)
 
     output_process(solution, file)
